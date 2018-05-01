57edb9b4d9e0c0e4259cf76fdfa63422c23cb56c
Unknown <DTruong054@west-mec.org>
2018-04-30 19:59:27 -0700

    Updated locations, added a notepad.js file(This is a notepad, not code.)

---
+++ notepad.js
@@ -0,0 +1,19 @@
+//This is a notepad, i got lazy to write in a real one
+
+//Help with finding the warehouse
+//Found one, its: 4441 W Polk St Phoenix, AZ 85043
+//Really cheap: $2.28 /SF/Yr, that comes out to about $3985.44
+//Looks alright but its the cheapest one we could buy without negotiateing and i'd rather not do that, unless one of you wants to do that. Cheap and big. 1,748 SF 1st Floor Ste 130
+
+// ========================Information=============================
+//Warehouse
+// Rental Rate
+// $2 .28 / SF / Yr
+// No.Dock - High Doors / Loading 5
+// Service Type Triple Net Office Size 1, 748 SF
+// Space Available 19, 511 SF Drive In Bay Count 1
+// Space Type Relet Floor 1 st Floor
+// Lease Term 1 - 10 Years Space Use Industrial
+// Date Available 30 Days• 1, 748 s.f.of new office• Five dock positions T5 energy efficient warehouse lighting• Clear height: 24’• Power: 200 amps, 277 / 480 V• Rail capable
+
+// ========================Information=============================
