710c87f5b4c02de9da9999ff08b480d93def6426
Unknown <DTruong054@west-mec.org>
2018-05-04 13:24:39 -0700

    Updated Notepad/agenda

--- Notepads/notepad.js
+++ Notepads/notepad.js
@@ -17,5 +17,3 @@
 // Date Available 30 Days• 1, 748 s.f.of new office• Five dock positions T5 energy efficient warehouse lighting• Clear height: 24’• Power: 200 amps, 277 / 480 V• Rail capable
 
 // ========================Information=============================
-
-// ========================Information=============================
