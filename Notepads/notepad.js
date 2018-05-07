2b69406fdc74fce8fbb4e14491de33d641d11445
Unknown <DTruong054@west-mec.org>
2018-05-07 15:02:57 -0700

    Worked on financial plan

--- Notepads/notepad.js
+++ Notepads/notepad.js
@@ -1,11 +1,23 @@
 //This is a notepad, i got lazy to write in a real one
 
+
+/*==================================Start of Notes==========================================*/
 //Help with finding the warehouse
 //Found one, its: 4441 W Polk St Phoenix, AZ 85043
 //Really cheap: $2.28 /SF/Yr, that comes out to about $3985.44
 //Looks alright but its the cheapest one we could buy without negotiateing and i'd rather not do that, unless one of you wants to do that. Cheap and big. 1,748 SF 1st Floor Ste 130
 
-// ========================Information=============================
+//If we have no financial backing for a year we would be in debt, by A LOT
+//We could live off for a month though
+//Lazy to do costs to see how long we can go without backing
+//Also we need to sell A LOT of products if we paying back Clawson 50k
+//Unless each t-shirt/mug/hat is like 1k per shirt/mug/hat
+//We could start selling stickers I guess, they would be cheap to buy/sell we could make money
+/*==================================End of Notes============================================*/
+
+/*==================================Start of Calculations===================================*/
+
+// ========================Warehouse=============================
 //Warehouse
 // Rental Rate
 // $2 .28 / SF / Yr
@@ -16,4 +28,58 @@
 // Lease Term 1 - 10 Years Space Use Industrial
 // Date Available 30 Days• 1, 748 s.f.of new office• Five dock positions T5 energy efficient warehouse lighting• Clear height: 24’• Power: 200 amps, 277 / 480 V• Rail capable
 
-// ========================Information=============================
+// ========================Warehouse=============================
+
+
+
+// ========================Internet=============================
+// W Telecom - Other Copper Wireline:
+// Has a great rate of 3mbps
+// Very cheap
+// $10 a month (WOOOO Good Deal)
+
+//Century Link
+//Has alright speeds
+//Great price if you planning to just have one or two devices on the internet at a single time
+//Affordable
+//45.00 a month
+
+
+// Cox Communications
+//Has the best speed
+//Somewhat affordable if we cut down on some expenses
+//Cox Internet Ultimate: $79.99 a month
+// ========================Internet=============================
+
+
+
+// ========================Electric=============================
+// About $2.00 per SF/month
+// Adds up to $3496.00
+// ========================Electric=============================
+
+
+
+
+// ========================Gas=============================
+// If we keep temp above 70deg in summer and under 60 in winter
+// Costs come to about under $50 ($48.92)
+// ========================Gas=============================
+
+
+
+// ========================Insurance=============================
+// Commercial General Liability Insurance: $400-$600
+// Workers' Compensation Insurance: Cost per $100 in wages: $0.89
+// ========================Insurance=============================
+
+
+
+// ========================Water=============================
+// $0.00295 per gallon, thats roughly 3cent for every 10 gallons
+// ========================Water=============================
+
+
+
+
+/*==================================End of Calculations===================================*/
